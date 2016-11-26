448b4c7767e0396829cbd78f7c3ea923e63246f7
Rohit Garg <rohitgarg19@gmail.com>
2016-11-26 20:44:46 +0530

    Adding configuration option for heroku RubyGems Path

--- webapp.js
+++ webapp.js
@@ -7,6 +7,7 @@ module.exports = {
       enum: ['2.6', '2.7', '3.3', 'whatever']
     },
     */
+    gemPath: '/app/vendor/bundle/ruby/2.0.0',
     test: {
       type: String,
       enum: ['none', 'bundle exec rspec', 'bundle exec rake test'],
